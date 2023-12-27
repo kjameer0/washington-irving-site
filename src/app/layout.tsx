4613660ef12b75f76333a2b4bf948111a4e8e0d2
Khalid Jameer <kjameer0@gmail.com>
2023-12-26 23:58:30 -0500

    test: attempting script to overwrite window history

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 import SideNavBar from "@/components/SideNavBar/SideNavBar";
 import MobileNavBar from "@/components/MobileNavBar/MobileNavBar";
@@ -33,6 +34,21 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <Script
+      id="redirect-script"
+        strategy="beforeInteractive"
+      >
+{(function direct(l): any {
+        if (l.search[1] === '/' ) {
+          let decoded = l.search.slice(1).split('&').map(function(s) {
+            return s.replace(/~and~/g, '&')
+          }).join('?');
+          window.history.replaceState(null, '',
+              l.pathname.slice(0, -1) + decoded + l.hash
+          );
+        }
+      }(window.location))}
+      </Script>
     </html>
   );
 }
