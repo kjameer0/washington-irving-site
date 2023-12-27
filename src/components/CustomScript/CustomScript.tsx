9696cd5cd40996e90536a7ff7ac52d88bff8a994
Khalid Jameer <kjameer0@gmail.com>
2023-12-27 16:31:37 -0500

    test: putting script into layout

--- src/components/CustomScript/CustomScript.tsx
+++ src/components/CustomScript/CustomScript.tsx
@@ -19,10 +19,19 @@ export default function CustomScript() {
             "",
             l.pathname.slice(0, -1) + decoded + l.hash
           );
-          console.log("complete");
         }
+        console.log("complete");
       }
     })();
+    let pathSegmentsToKeep = 0;
+      let l = window.location;
+      l.replace(
+        l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
+        l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/?/' +
+        l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
+        (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+        l.hash
+      );
   }, []);
   return <script></script>;
 }
